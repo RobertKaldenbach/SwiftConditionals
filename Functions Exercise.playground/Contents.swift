0fc070aa6d45684049d328031ab8c175c4873b1b
Robert Kaldenbach <Bobert@Roberts-Mac-mini.local>
2016-01-31 21:16:00 -0500

    Finished Excersise

--- Functions Exercise.playground/Contents.swift
+++ Functions Exercise.playground/Contents.swift
@@ -2,4 +2,46 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// "add" Function take two Double and returns the sum
+
+func add(numA:Double,numB: Double) ->Double{
+    return numA+numB
+}
+
+//Test of add
+add (12, numB:15)
+
+
+//"subtract" function two Int's return the difference
+
+func subtract(numA: Int, numB: Int) ->Int{
+    return numA - numB
+}
+
+//Test of Subtract
+subtract(12, numB: 54)
+
+
+//"multiply" function takes two floats return the product
+
+func multiply(numA: Float, numB: Float) ->Float{
+    return numA * numB
+}
+
+multiply(23.44, numB:54.68)
+
+
+//"divide" function takes to Double and return the result
+
+func divide(numA: Double, numB: Double) ->Double{
+    return numA / numB
+}
+
+//Test divide
+divide(35, numB: 58)
+
+
+
+
+
+
