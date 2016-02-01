176b8301bebb2451fe2473465d715ed708d4513a
Robert Kaldenbach <Bobert@Roberts-Mac-mini.local>
2016-01-31 21:00:23 -0500

    finished playground for functions

--- uptown-function-you-up.playground/Contents.swift
+++ uptown-function-you-up.playground/Contents.swift
@@ -27,4 +27,25 @@ if canPurchase(itemAmount){
     print("Insufficent Funds")
 }
 
+var name = "sweeny todd"
+
+func getUpperVersion(inputStr: String) -> String{
+    return inputStr.uppercaseString
+}
+
+var nameUpper = getUpperVersion(name)
+
+func declareWinner(playerAScore: Int, playerBScore: Int) {
+    if playerAScore > playerBScore {
+        print("Player A Wins")
+    } else if playerBScore  > playerAScore {
+        print("Player B Wins")
+    } else {
+        print("The game is at a stand still!!")
+    }
+}
+
+declareWinner(50,playerBScore: 50)
+
+
 
